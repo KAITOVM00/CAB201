19faeb5cd6dc76c5ce3ce1b63a62c663e1d550e3
Peter Le <petersmileyface@gmail.com>
2015-06-09 08:40:58 +1000

    Implemented Reverse method
    Changed access modifier to private for all existing methods

--- finals-sample/q2/a/Program.cs
+++ finals-sample/q2/a/Program.cs
@@ -9,15 +9,27 @@ namespace q2_a
     {
         static void Main(string[] args)
         {
-
+            
         }
 
-        public static void Reverse(double[] values, int start, int finish)
+        private static double[] Reverse(double[] values, int start, int finish)
         {
+            int n = finish - start + 1;
+            int firstIndex = start;
+            int secondIndex = finish;
+            int range = n / 2;
 
+            for (int i = 0; i < range; i++)
+            {
+                Swap(values, firstIndex, secondIndex);
+                firstIndex++;
+                secondIndex--;
+            }
+
+            return values;
         }
 
-        public static double[] Swap(double[] numbers, int oneIndex, int otherIndex)
+        private static double[] Swap(double[] numbers, int oneIndex, int otherIndex)
         {
             double temp = numbers[oneIndex];
             numbers[oneIndex] = numbers[otherIndex];
