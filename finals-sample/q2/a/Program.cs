202126ee1a9026ddefb83ad34a00cd9d0e89165a
Peter Le <petersmileyface@gmail.com>
2015-06-09 09:40:49 +1000

    Created Reverse extension method

--- finals-sample/q2/a/Program.cs
+++ finals-sample/q2/a/Program.cs
@@ -12,29 +12,12 @@ namespace q2_a
             double[] data = { 8.5, 12.0, 23.2, 18.0, 15.5, 5.0, 10.5 };
             OutputData(data);
 
-            data = Reverse(data, 2, 5);
+            data.Reverse(2, 5);
             OutputData(data);
 
             Console.ReadKey();
         }
-
-        private static double[] Reverse(double[] values, int start, int finish)
-        {
-            int n = finish - start + 1;
-            int firstIndex = start;
-            int secondIndex = finish;
-            int range = n / 2;
-
-            for (int i = 0; i < range; i++)
-            {
-                values.Swap(firstIndex, secondIndex);
-                firstIndex++;
-                secondIndex--;
-            }
-
-            return values;
-        }
-
+        
         private static void OutputData(double[] numbers)
         {
             foreach (double n in numbers)
