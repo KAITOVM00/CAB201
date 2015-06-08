46b88f5c3981e26199b8d32dfa28728d0ce9bc0e
Peter Le <petersmileyface@gmail.com>
2015-06-09 09:41:17 +1000

    Added ExtensionMethods

---
+++ finals-sample/q2/a/ExtensionMethods.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q2_a
+{
+    public static class ExtensionMethods
+    {
+        public static double[] Reverse(this double[] numbers, int start, int finish)
+        {
+            int n = finish - start + 1;
+            int firstIndex = start;
+            int secondIndex = finish;
+            int range = n / 2;
+
+            for (int i = 0; i < range; i++)
+            {
+                numbers.Swap(firstIndex, secondIndex);
+                firstIndex++;
+                secondIndex--;
+            }
+
+            return numbers;
+        }
+
+        public static double[] Swap(this double[] numbers, int oneIndex, int otherIndex)
+        {
+            double temp = numbers[oneIndex];
+            numbers[oneIndex] = numbers[otherIndex];
+            numbers[otherIndex] = temp;
+
+            return numbers;
+        }
+    }
+}
