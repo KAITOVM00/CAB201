a61348e8e6518910fd742ca3f4fe5764af7cfd9a
Peter Le <petersmileyface@gmail.com>
2015-06-09 13:48:09 +1000

    Added extension methods
    Implemented IsSorted extension method

--- finals-sample/q2/b/ExtensionMethods.cs
+++ finals-sample/q2/b/ExtensionMethods.cs
@@ -8,6 +8,46 @@ namespace q2_b
 {
     public static class ExtensionMethods
     {
+        public static Boolean IsSorted(this int[] numbers)
+        {
+            Boolean isAscending = false;
+            Boolean isDescending = false;
 
+            List<int> sortedNumbers = new List<int>();
+            foreach (int n in numbers)
+            {
+                sortedNumbers.Add(n);
+            }
+
+            sortedNumbers = (sortedNumbers.OrderBy(i => i)).ToList();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == sortedNumbers[i] && i >= numbers.Length - 1)
+                {
+                    isAscending = true;
+                }
+                else if (numbers[i] != sortedNumbers[i])
+                {
+                    isAscending = false;
+                    break;
+                }
+            }
+            
+            sortedNumbers = (sortedNumbers.OrderByDescending(i => i)).ToList();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == sortedNumbers[i] && i >= numbers.Length - 1)
+                {
+                    isDescending = true;
+                }
+                else if (numbers[i] != sortedNumbers[i])
+                {
+                    isDescending = false;
+                    break;
+                }
+            }
+
+            return isAscending || isDescending;
+        }
     }
 }
--- finals-sample/q2/b/Program.cs
+++ finals-sample/q2/b/Program.cs
@@ -10,6 +10,15 @@ namespace q2_b
     {
         static void Main(string[] args)
         {
+            int[] numbers = { 0, 1, 2, 3, 4, 5, 6 };
+            foreach (int n in numbers)
+            {
+                Console.Write(n.ToString() + " ");
+            }
+            Console.WriteLine("\n");
+            Console.Write(numbers.IsSorted().ToString());
+                        
+            Console.ReadKey();
         }
     }
 }
