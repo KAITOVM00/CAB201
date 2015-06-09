25b1655413e86c97b1dd8d9ea138c841c09ef054
Peter Le <petersmileyface@gmail.com>
2015-06-09 19:49:07 +1000

    Initial commit

---
+++ finals-sample/q1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] anArray = { 1, 9, 1, 9, 9, 5, 7, 7, 4 };
+            Console.Write(FirstPairOfDuplicates(anArray).ToString());
+            Console.ReadKey();
+        }
+
+        private static int FirstPairOfDuplicates(int[] anArray)
+        {
+            
+        }
+    }
+}
